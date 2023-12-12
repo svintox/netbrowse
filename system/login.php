17b5935b8daf7e636da6d151b9c3a92926fe4c46
Frode Aabak <frode_aabak@hotmail.com>
2023-12-12 22:37:13 +0100

    Create login.php

---
+++ system/login.php
@@ -0,0 +1,6 @@
+<h1 style="text-align:center"><span class="material-icons">login</span> Login</h1>
+<form action="index.php" method="post" style="text-align:center">
+    <input type="text" name="username" placeholder="Username"><br><br>
+    <input type="password" name="password" placeholder="Password"><br><br>
+    <input type="submit" value="Login">
+</form>
