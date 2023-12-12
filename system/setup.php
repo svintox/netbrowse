4124b9ad9f002be3093f1e2d4578a55079193be9
Frode Aabak <frode_aabak@hotmail.com>
2023-12-12 22:08:34 +0100

    Create setup.php

---
+++ system/setup.php
@@ -0,0 +1,6 @@
+<h1><span class="material-icons">toggle_on</span> Setup</h1>
+<form action="index.php" method="post">
+    <input type="text" name="login" placeholder="Login"><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <input type="submit" value="Create">
+</form>
